0a6a56a363c4243025aad3a9ee89427652ba7f4a
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-04-02 16:26:07 -0300

    feat(*): criação do usuario após o login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,27 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  jwt: {
+    signingKey: process.env.JWT_SINGIN_KEY
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      console.log(email);
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true;
+      } catch(err) {
+        console.log(err);
+        return false;
+      }
+    }
+  }
 })
