1771a8fbeefc22ac330fb539c9b8b61dee1892a7
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-04-14 09:35:18 -0300

    feat(posts): criando pagina de lista de posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,38 @@
+import Head from "next/head";
+
+import styles from './styles.module.scss';
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a>
+            <time>12 de março de 2021</time>
+            <strong>Title</strong>
+            <p>Description</p>
+          </a>
+          <a>
+            <time>12 de março de 2021</time>
+            <strong>Title</strong>
+            <p>Description</p>
+          </a>
+          <a>
+            <time>12 de março de 2021</time>
+            <strong>Title</strong>
+            <p>Description</p>
+          </a>
+          <a>
+            <time>12 de março de 2021</time>
+            <strong>Title</strong>
+            <p>Description</p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
