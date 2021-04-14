e94d8cfb756bbfd096fe988adf7e898764485377
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-04-14 10:01:35 -0300

    feat(*): fazendo a integracao com o prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,6 +1,9 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Prismic from '@prismicio/client';
 
 import styles from './styles.module.scss';
+import { getPrismicClient } from "../../services/prismic";
 
 export default function Posts() {
   return (
@@ -35,4 +38,21 @@ export default function Posts() {
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post')
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100
+  });
+
+  console.log(response);
+
+  return {
+    props: {}
+  }
 }
