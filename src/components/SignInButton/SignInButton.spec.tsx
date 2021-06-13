60ed95914ede7b667d44d3c80b1fb855a683c76b
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-06-13 19:10:06 -0300

    feat(*): criacao de teste do comp. signinbutton

---
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import { mocked } from 'ts-jest/utils'
+import { useSession } from 'next-auth/client';
+import { SignInButton } from '.';
+
+jest.mock('next-auth/client')
+
+describe('ActiveLink component', () => {
+  it('renders correctly when user is not authenticated', () => {
+    const useSessionMocked = mocked(useSession);
+    useSessionMocked.mockReturnValueOnce([null, false])
+
+    render(<SignInButton />)
+    const text: any = 'Sign in with Github'
+    expect(screen.getByText(text)).toBeInTheDocument();
+  });
+  it('renders correctly when user is authenticated', () => {
+    const useSessionMocked = mocked(useSession);
+    useSessionMocked.mockReturnValueOnce([{
+      user: {
+        name: 'John Doe'
+      },
+      expires: '1d'
+    }, false])
+    render(<SignInButton />)
+    const text: any = 'John Doe'
+    expect(screen.getByText(text)).toBeInTheDocument();
+  });
+})
